409791adb02e20212c65c78fbc9fc3ceed9d4471
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-27 17:10:36 +0300

    FootballMatch

--- src/com/company/FootballMatch.java
+++ src/com/company/FootballMatch.java
@@ -1,11 +1,11 @@
 package com.company;
-import java.util.Scanner; // импорт сканера
+import java.util.Scanner; 
 
 
 public class FootballMatch {
 
     static Scanner scan = new Scanner(System.in);
-    public int MatchOutcome; // переменная исхода матча
+    public int MatchOutcome; 
 
     public int getInt(){//валидатор ввода данных
         int num;
@@ -19,21 +19,21 @@ public class FootballMatch {
         return num;
     }
 
-    //метод подсчёта исхода матча RealOutcome 1 и 2 - реальный счёт, Forecast 1 и 2 - прогноз пользователя
+   
     public int MatchForecast(int RealOutcome1,int RealOutcome2,int Forecast1, int Forecast2){
-        if((RealOutcome1 == Forecast1 && RealOutcome2 == Forecast2))//пользователь угадал, исход = 2
+        if((RealOutcome1 == Forecast1 && RealOutcome2 == Forecast2))
         {
             MatchOutcome = 2;
         }
         else {
-            //проверка выиграла ли команда пользователя
+      
             if ((RealOutcome1 >= RealOutcome2 && Forecast1 >= Forecast2) ||
                     ((RealOutcome1 <= RealOutcome2 && Forecast1 <= Forecast2)))
-                MatchOutcome = 1;//команда пользователя не выиграла, исход = 1
-            else//команда пользователя не выиграла, исход = 0
+                MatchOutcome = 1;
+            else/
                 MatchOutcome = 0;
         }
-        return MatchOutcome;//возвращаем исход
+        return MatchOutcome;
 
     }
 }
