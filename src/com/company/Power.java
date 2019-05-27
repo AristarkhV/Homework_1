05fa141fd4acbf0b3623c0fdb2dba1a9cf3c850c
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-27 17:11:46 +0300

    Power

--- src/com/company/Power.java
+++ src/com/company/Power.java
@@ -1,28 +1,28 @@
 package com.company;
-import java.util.Scanner; // импорт сканера
+import java.util.Scanner; 
 
 public class Power {
 
     static Scanner scan = new Scanner(System.in);
 
-    public int getInt(){//валидатор ввода данных
+    public int getInt(){
         int num;
         if(scan.hasNextInt()){
             num = scan.nextInt();
         } else {
             System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
             scan.next();
-            num = getInt();//рекурсия
+            num = getInt();
         }
         return num;
     }
 
-    static int getPower(int Num, int Pow){ // Num - число, Pow - степень
-        int result = 1;//число в 0 степени = 1
-        for (int i=1; i<=Pow; i++){//в цикле умножаем число Num количество Pow раз само на себя
+    static int getPower(int Num, int Pow){ 
+        int result = 1;
+        for (int i=1; i<=Pow; i++){
             result = result*Num;
         }
-        return result;//результат возведения в степень
+        return result;
     }
 
 
